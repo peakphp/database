5827f08e3b8d1d6b49242cb8152895e5564477c3
Franck <snake386@hotmail.com>
2019-04-28 20:51:02 -0400

    added EnvConfigFactory

---
+++ src/Phinx/EnvConfigFactory.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Peak\Database\Phinx;
+
+use \Closure;
+
+class EnvConfigFactory implements EnvConfigInterface
+{
+    /**
+     * @var string
+     */
+    private $envName;
+
+    /**
+     * @var Closure
+     */
+    private $envConfigClosure;
+
+    /**
+     * @param string $envName
+     * @param array $envConfig
+     */
+    public function __construct(string $envName, Closure $envConfigClosure)
+    {
+        $this->envName = $envName;
+        $this->envConfigClosure = $envConfigClosure;
+    }
+
+    /**
+     * @return array
+     */
+    public function getConfig(): array
+    {
+        $fn = $this->envConfigClosure;
+        return [
+            $this->envName => $fn()
+        ];
+    }
+}
