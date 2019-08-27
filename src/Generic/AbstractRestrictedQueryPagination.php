ee0ef51ebb338d05d11a80a7a265b17e84ae3456
Franck <snake386@hotmail.com>
2019-08-27 13:38:44 -0400

    getOrderBy() can take now also handle column names with table prefix

--- src/Generic/AbstractRestrictedQueryPagination.php
+++ src/Generic/AbstractRestrictedQueryPagination.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Peak\Database\Generic;
 
+use \Exception;
+
 abstract class AbstractRestrictedQueryPagination extends QueryPagination
 {
     /**
@@ -18,26 +20,30 @@ abstract class AbstractRestrictedQueryPagination extends QueryPagination
 
     /**
      * @return string
-     * @throws \Exception
+     * @throws Exception
      */
     public function getOrderBy(): string
     {
         $value = parent::getOrderBy();
+        $valueParts = explode('.', str_replace('`', '', $value));
         if (!in_array($value, $this->allowedColumns)) {
-            throw new \Exception('Expected orderBy to be '.implode(' or ', $this->allowedColumns).'. Received "'.$value.'"');
+            // check for table prefixed column name (ex: table.column)
+            if (count($valueParts) < 2 || !in_array($valueParts[1], $this->allowedColumns)) {
+                throw new Exception('Expected orderBy to be '.implode(' or ', $this->allowedColumns).'. Received "'.$value.'"');
+            }
         }
         return $value;
     }
 
     /**
      * @return string
-     * @throws \Exception
+     * @throws Exception
      */
     public function getDirection(): string
     {
         $value = parent::getDirection();
         if (!in_array($value, $this->allowedDirections)) {
-            throw new \Exception('Expected direction to be '.implode(' or ', $this->allowedDirections).'. Received "'.$value.'"');
+            throw new Exception('Expected direction to be '.implode(' or ', $this->allowedDirections).'. Received "'.$value.'"');
         }
         return $value;
     }
